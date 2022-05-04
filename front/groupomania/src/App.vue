cd05fadda7225f2d3a0dc8d2c6fdc7675bc391c4
Siod <theodupontpro@gmail.com>
2022-05-05 00:36:39 +0200

    add login and signup form (dynamic function for switch not working)

--- front/groupomania/src/App.vue
+++ front/groupomania/src/App.vue
@@ -1,33 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img id="user" src="./assets/Groupomania-user.svg" alt="groupomania user" />
+    <LoginPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import LoginPage from "./components/LoginPage.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    LoginPage,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./css/main.css";
+@import "./css/login.css";
 </style>
---
+++ front/groupomania/src/components/LoginPage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="connection">
+    <!-- login -->
+    <div class="login">
+      <form id="form-login" class="form" method="get">
+        <div v-if="connectionMethod === 'Signup'" class="wrapper-name">
+          <input id="name" type="name" placeholder="name" />
+          <input id="lastname" type="lastname" placeholder="lastname" />
+        </div>
+        <select v-if="connectionMethod === 'Signup'" id="job" class="input">
+          <option value="">Job</option>
+          <option value="dev">dev</option>
+          <option value="graphiste">graphiste</option>
+          yes
+        </select>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="groupomania@social.network"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+        />
+        <div class="wrapper-submit">
+          <a href="" id="type-of-connection" @click="connection()">{{
+            otherMethod
+          }}</a>
+          <input id="submit" type="submit" :value="connectionMethod" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      connectionMethod: "Signup",
+      otherMethod: "signup",
+    };
+  },
+  methods: {
+    connection() {
+      if (this.connectionMethod === "Login") {
+        this.connectionMethod = "Signup";
+        this.otherMethod = "login";
+      } else {
+        this.connectionMethod = "Login";
+        this.otherMethod = "signup";
+      }
+      // alert(this.connectionMethod);
+    },
+  },
+};
+</script>
